82581bd52dca71781fed81b22e83d39bf4219b56
Javier Rodriguez <javicerodriguez@gmail.com>
2023-07-31 01:12:23 -0300

    add: some info about us

--- pages/1_😀_Sobre_nosotros.py
+++ pages/1_😀_Sobre_nosotros.py
@@ -2,15 +2,34 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Nosotros",
-    page_icon="👋",
+    page_icon="😀",
     layout="wide",
 )
 
+st.header("😀 Sobre nosotros")
 st.write("""
-# About
-
-| foto | foto |
-|------|------|
-|Lucas Oliaro Vera | Javier Rodriguez |
+Somos estudiantes de Licenciatura en Ciencia de Datos de UNSAM e hicimos este presentación de datos para aplicar nuestro
+aprendizaje en el primer año de la carrera. Agradecemos al equipo de Rodrigo Díaz por alentarnos a participar y demostrar
+que con datos podemos hacer muchas cosas interesantes! 😄
 """)
+
+col1, col2, col3 = st.columns(3)
+
+col1.subheader("Lucas Oliaro Vera")
+col1.write("""
+[GitHub](https://github.com/lucas-oliaro) - [LinkedIn](https://www.linkedin.com/in/lucas-oliaro/)
+""")
+col1.image("https://avatars.githubusercontent.com/u/81052967?v=4")
+
+col2.subheader("Javier Rodriguez")
+col2.write("""
+[GitHub](https://github.com/JaviCeRodriguez) - [LinkedIn](https://www.linkedin.com/in/rodriguezjavierc/)
+""")
+col2.image("https://avatars.githubusercontent.com/u/68615684?v=4")
+
+col3.subheader("Gabriel Arnesano")
+col3.write("""
+[GitHub](https://github.com/gabrielArne) - [LinkedIn](https://www.linkedin.com/in/gabriel-arnesano/)
+""")
+col3.image("https://avatars.githubusercontent.com/u/115743146?v=4")
 
