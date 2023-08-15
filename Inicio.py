3325f22d9a86a8f2ff6ee5fc8a58df9bf9cc628f
Javier Rodriguez <javicerodriguez@gmail.com>
2023-08-15 19:15:02 -0300

    add: validations for missing data in KPIs

--- Inicio.py
+++ Inicio.py
@@ -68,32 +68,41 @@ with col1_today:
         df_demanda.dropna(inplace=True)
         last_row_dem = df_demanda.iloc[-1]
 
-        col1_m1, col1_m2, col1_m3 = st.columns(3)
+        if last_row_dem.empty or df_demanda.empty:
+            st.write("No hay datos suficientes para mostrar este KPI")
 
-        diff_m1_1 = round(last_row_dem['demHoy'] - last_row_dem['demAyer'], 3)
-        col1_m1.metric(
-            label="Demanda hoy vs ayer",
-            value=f"{last_row_dem['demHoy']} GW",
-            delta=f"{diff_m1_1} GW",
-            delta_color="inverse"
-        )
-        diff_m1_2 = round(last_row_dem['demHoy'] - last_row_dem['demPrevista'], 3)
-        col1_m2.metric(
-            label="Demanda hoy vs prevista",
-            value=f"{last_row_dem['demHoy']} GW",
-            delta=f"{diff_m1_2} GW",
-            delta_color="inverse"
-        )
-        diff_m1_3 = round(last_row_dem['tempHoy'] - last_row_dem['tempAyer'], 3)
-        col1_m3.metric(
-            label="Temperatura hoy vs ayer",
-            value=f"{last_row_dem['tempHoy']} °C",
-            delta=f"{diff_m1_3} °C",
-            delta_color="inverse"
-        )
+        if len(last_row_dem) > 0:
+            col1_m1, col1_m2, col1_m3 = st.columns(3)
 
-        with st.expander("Ver datos de demanda"):
-            st.dataframe(df_demanda)
+            if 'demHoy' in last_row_dem.index and 'demAyer' in last_row_dem.index:
+                diff_m1_1 = round(last_row_dem['demHoy'] - last_row_dem['demAyer'], 3)
+                col1_m1.metric(
+                    label="Demanda hoy vs ayer",
+                    value=f"{last_row_dem['demHoy']} GW",
+                    delta=f"{diff_m1_1} GW",
+                    delta_color="inverse"
+                )
+
+            if 'demHoy' in last_row_dem.index and 'demPrevista' in last_row_dem.index:
+                diff_m1_2 = round(last_row_dem['demHoy'] - last_row_dem['demPrevista'], 3)
+                col1_m2.metric(
+                    label="Demanda hoy vs prevista",
+                    value=f"{last_row_dem['demHoy']} GW",
+                    delta=f"{diff_m1_2} GW",
+                    delta_color="inverse"
+                )
+
+            if 'tempHoy' in last_row_dem.index and 'tempAyer' in last_row_dem.index:
+                diff_m1_3 = round(last_row_dem['tempHoy'] - last_row_dem['tempAyer'], 3)
+                col1_m3.metric(
+                    label="Temperatura hoy vs ayer",
+                    value=f"{last_row_dem['tempHoy']} °C",
+                    delta=f"{diff_m1_3} °C",
+                    delta_color="inverse"
+                )
+
+            with st.expander("Ver datos de demanda"):
+                st.dataframe(df_demanda)
 
 with col2_today:
     st.write(f"### Generación de {region_selected.get('nombre')} (ahora)")
@@ -101,6 +110,10 @@ with col2_today:
     response_gen = requests.get(api_generacion + str(region_selected.get('id')))
     if response_dem.status_code == 200:
         generacion = list(response_gen.json())
+
+        if len(generacion) == 0:
+            st.write("No hay datos disponibles para mostrar este KPI")
+
         if len(generacion) > 0:
             df_generacion = pd.DataFrame(generacion)
             df_generacion['fecha'] = pd.to_datetime(df_generacion['fecha'])
@@ -109,35 +122,42 @@ with col2_today:
             last_row_gen_1 = df_generacion.iloc[-1]
             last_row_gen_2 = df_generacion.iloc[-2]
 
+            if last_row_gen_1.empty or last_row_gen_2.empty or df_generacion.empty:
+                st.write("No hay datos suficientes para mostrar este KPI")
+
             col2_m1, col2_m2, col2_m3, col2_m4 = st.columns(4)
 
-            diff_m2_1 = round(last_row_gen_1['hidraulico'] - last_row_gen_2['hidraulico'], 3)
-            col2_m1.metric(
-                label="💧 Hidráulico",
-                value=f"{last_row_gen_1['hidraulico']} GW",
-                delta=f"{diff_m2_1} GW",
-            )
+            if 'hidraulico' in last_row_gen_1.index and 'hidraulico' in last_row_gen_2.index:
+                diff_m2_1 = round(last_row_gen_1['hidraulico'] - last_row_gen_2['hidraulico'], 3)
+                col2_m1.metric(
+                    label="💧 Hidráulico",
+                    value=f"{last_row_gen_1['hidraulico']} GW",
+                    delta=f"{diff_m2_1} GW",
+                )
 
-            diff_m2_2 = round(last_row_gen_1['termico'] - last_row_gen_2['termico'], 3)
-            col2_m2.metric(
-                label="🌡️ Térmico",
-                value=f"{last_row_gen_1['termico']} GW",
-                delta=f"{diff_m2_2} GW",
-            )
+            if 'termico' in last_row_gen_1.index and 'termico' in last_row_gen_2.index:
+                diff_m2_2 = round(last_row_gen_1['termico'] - last_row_gen_2['termico'], 3)
+                col2_m2.metric(
+                    label="🌡️ Térmico",
+                    value=f"{last_row_gen_1['termico']} GW",
+                    delta=f"{diff_m2_2} GW",
+                )
 
-            diff_m2_3 = round(last_row_gen_1['nuclear'] - last_row_gen_2['nuclear'], 3)
-            col2_m3.metric(
-                label="☢️ Nuclear",
-                value=f"{last_row_gen_1['nuclear']} GW",
-                delta=f"{diff_m2_3} GW",
-            )
+            if 'nuclear' in last_row_gen_1.index and 'nuclear' in last_row_gen_2.index:
+                diff_m2_3 = round(last_row_gen_1['nuclear'] - last_row_gen_2['nuclear'], 3)
+                col2_m3.metric(
+                    label="☢️ Nuclear",
+                    value=f"{last_row_gen_1['nuclear']} GW",
+                    delta=f"{diff_m2_3} GW",
+                )
 
-            diff_m2_4 = round(last_row_gen_1['renovable'] - last_row_gen_2['renovable'], 3)
-            col2_m4.metric(
-                label="🍃 Renovable",
-                value=f"{last_row_gen_1['renovable']} GW",
-                delta=f"{diff_m2_4} GW",
-            )
+            if 'renovable' in last_row_gen_1.index and 'renovable' in last_row_gen_2.index:
+                diff_m2_4 = round(last_row_gen_1['renovable'] - last_row_gen_2['renovable'], 3)
+                col2_m4.metric(
+                    label="🍃 Renovable",
+                    value=f"{last_row_gen_1['renovable']} GW",
+                    delta=f"{diff_m2_4} GW",
+                )
 
             with st.expander("Ver datos de generación"):
                 st.dataframe(df_generacion)
